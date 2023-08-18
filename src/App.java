ebd745e2215c29c7d73e2480be33472c47f30840
amanib <amanibobo1@gmail.com>
2023-08-18 14:07:16 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args)  {
+        for(int i = 0; i < 15; i++) {
+            System.out.println(  3 + -5 * i );
+        }
+        for(int i = 2; i < 6; i++) {
+            System.out.println( i + 4 );
+        }
+    }
+}
